3da2abe37b1e3228bc81f86c894cb284c6eef8de
Tunjay <thuseynov2020@ada.edu.az>
2018-10-01 00:27:29 +0400

    Version 2

---
+++ ClientBlueLightStaffing/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientBlueLightStaffing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        string sMsg;
+
+        public ActionResult Send(string name, string phone, string email, string message) {
+
+            string smtpAddress = "relay-hosting.secureserver.net";
+            int portNumber = 25;
+            bool enableSSL = false;
+
+            string emailFrom = "tuncayhuseynov@gmail.com";
+            string password = "5591980supertun";
+            string emailTo = "rachel@bluelightstaffing.com";
+            
+
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(emailFrom);
+                mail.To.Add(emailTo);
+                mail.Subject = "Support Desk Message";
+
+
+                sMsg = "Name : " + name + "\nEmail : " + email + "\nPhone : " + phone + "\nMessage : " + message;
+
+
+
+
+
+                mail.Body = sMsg;
+                mail.IsBodyHtml = false;
+                // Can set to false, if you are sending pure text.
+
+
+
+                using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                {
+                    smtp.Credentials = new NetworkCredential(emailFrom, password);
+                    smtp.EnableSsl = enableSSL;
+                    smtp.Send(mail);
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
---
+++ ClientBlueLightStaffing/obj/Release/Package/PackageTmp/Public/js/js.js
@@ -0,0 +1,99 @@
+jQuery(document).ready(function ($) {
+
+
+   $(window).stellar();
+	$(".knob").knob({
+                "fgColor":"#ff6600"
+                });
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+	
+	/**/	
+	if (mywindow.scrollTop() < 1) {
+		$('.navigation li[data-slide="1"]').addClass('active');
+	}
+	/**/
+
+    slide.waypoint(function (event, direction) {
+
+        dataslide = $(this).attr('data-slide');
+
+        if (direction === 'down') {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+			
+			$('.navigation li[data-slide="1"]').removeClass('active');
+			
+        }
+        else {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+        }
+
+    });
+ 
+    mywindow.scroll(function () {
+        if (mywindow.scrollTop() == 0) {
+            $('.navigation li[data-slide="1"]').addClass('active');
+            $('.navigation li[data-slide="2"]').removeClass('active');
+        }
+    });
+
+    /*function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top + 4
+        }, 2000, 'easeInOutQuint');
+    }*/
+	
+	function goToByScroll(dataslide) {
+		var goal = $('.slide[data-slide="' + dataslide + '"]').offset().top;
+		if (mywindow.scrollTop()<goal) {
+			var goalPx = goal + 5;
+		} else {
+			var goalPx = goal - 50;
+		}
+        htmlbody.animate({
+            scrollTop: goalPx
+        }, 2000, 'easeInOutQuint');
+    }
+
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+
+    });
+	
+	//prettyPhoto
+		$("a[rel^='prettyPhoto']").prettyPhoto();
+	
+	// Sticky Navigation	
+		$(".menu").sticky({topSpacing:0});
+	
+	
+	$("#slide1, #slide5, #slide7").each(function () {
+        var slide_h = $(this).height();
+		
+		$(this).css('background-size', '100% '+slide_h+'px');
+		
+    });
+	
+	
+	
+	
+	
+});
+
+
+
+
+
+
+
